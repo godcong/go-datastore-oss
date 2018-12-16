2f43b3e1f8cd7d4810f5b363dab66e0318d5fc41
godcong <jumbycc@163.com>
2018-12-16 16:27:52 +0800

    add oss

--- oss.go
+++ oss.go
@@ -1,11 +1,30 @@
 package oss
 
+import (
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
 type Config struct {
+	Endpoint        string
+	AccessKeyID     string
+	AccessKeySecret string
+	BucketName      string
 }
 
-type Oss struct {
+type OssBucket struct {
+	Config
+	Client *oss.Client
 }
 
-func NewOssDatastore(config *Config) {
+func NewOssBucket(config Config, client *oss.Client) *OssBucket {
+	return &OssBucket{Config: config, Client: client}
+}
 
+func NewOssDatastore(config Config) (*OssBucket, error) {
+	client, err := oss.New(config.Endpoint, config.AccessKeyID, config.AccessKeySecret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new client: %s", err)
+	}
+	return NewOssBucket(config, client), nil
 }
